90bfbf038199f786c96cba6e7d4415e85b2607a4
Milen Georgiev <milendg99@gmail.com>
2019-11-05 12:04:15 +0000

    User authentication added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/home'
+  devise_for :users
+  get 'home/index'
   get 'home/contacts'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => redirect('home/index')
 end
