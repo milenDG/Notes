27d6daf7f8c79ddebb0f61ed04a2998aebadec9a
Milen Georgiev <milendg99@gmail.com>
2019-11-18 20:52:29 +0000

    Security of other users items added. Message of parameter tampering attempt added. Buttons fixed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def not_found
-    raise ActionController::RoutingError, 'Not Found'
+    flash[:alert] = "There is no such item."
+    redirect_to root_path
   end
 
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth) }
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth) }
+  end
 end
