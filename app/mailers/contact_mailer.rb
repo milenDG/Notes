d25472e73867bc43de84ed73077399e94faf7a7e
Milen Georgiev <mg00929@surrey.ac.uk>
2019-12-12 21:37:15 +0000

    Added mailer unit tests

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,10 +2,10 @@ class ContactMailer < ApplicationMailer
 
   # Method creating an email and sending it.
   def contact_email(email, name, telephone, title, message)
-    @title = title
     @email = email
     @name = name
     @telephone = telephone
+    @title = title
     @message = message
 
     mail cc: @email, subject: title
